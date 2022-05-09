b3c2afa1b37d17a050b27272d57e15e5de1e09af
feiqiong-deng <feiqiong.deng@outlook.com>
2022-05-09 16:18:31 -0400

    Add tags

---
+++ src/test/java/stepDefinitions/TestRunner_RunWithSingleTag.java
@@ -0,0 +1,13 @@
+package stepDefinitions;
+
+import org.junit.runner.RunWith;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features="src/test/resources/FeatureWithTags", 
+glue="stepDefinitions",tags="@smoke or @regression")
+public class TestRunner_RunWithSingleTag {
+
+}
